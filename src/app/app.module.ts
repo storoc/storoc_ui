00f21b9a1cec298188157ac9de83245d7633b40e
jfaro <jfaro@umich.edu>
2020-07-14 00:28:52 -0400

    pre angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
 
@@ -17,6 +18,7 @@ import { SearchBoxComponent } from './search-box/search-box.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { FooterComponent } from './footer/footer.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 import { MapComponent } from './map/map.component';
 
 @NgModule({
@@ -26,6 +28,7 @@ import { MapComponent } from './map/map.component';
     SearchBoxComponent,
     SearchResultsComponent,
     FooterComponent,
+    SideBarComponent,
     MapComponent,
   ],
   imports: [

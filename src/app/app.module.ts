db14ab91efa34ba5b543d671e88ed22bceacb5bd
jfaro <jfaro@umich.edu>
2020-07-17 11:23:22 -0400

    updated production environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AgmCoreModule} from '@agm/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { GooglePlaceModule } from "ngx-google-places-autocomplete";
+
+// Componenet imports
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
@@ -12,12 +14,12 @@ import { MapComponent } from './map/map.component';
 import { FooterComponent } from './footer/footer.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
+
 import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     HomeComponent,
     SideBarComponent,
     MapComponent,
@@ -33,7 +35,9 @@ import { environment } from 'src/environments/environment';
     AgmCoreModule.forRoot({
       apiKey: environment.googleApiKey,
       libraries: ['places']
-    })
+    }),
+    HttpClientModule,
+    GooglePlaceModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import PlaceResult = google.maps.places.PlaceResult;
 
 @Component({
   selector: 'app-search-box',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBoxComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+
   }
 
 }

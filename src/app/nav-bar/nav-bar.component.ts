c4d7319d50b41cfb954fc3bcdf32ac583909b240
jfaro <jfaro@umich.edu>
2020-08-12 14:25:09 -0400

    updated nav bar logic to clear page on login

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchResultService } from '../search-result.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchResultService: SearchResultService) { }
 
   ngOnInit(): void {
+  }
+
+  resetPage() {
+    this.searchResultService.clearData();
   }
 
 }
